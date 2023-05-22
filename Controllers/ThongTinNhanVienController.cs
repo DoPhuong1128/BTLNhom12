55f8d5294182f2d64b85b84a70988a97c3a33aac
DoPhuong1128 <mameo4869@gmail.com>
2023-05-22 16:11:49 +0700

    excels

--- Controllers/ThongTinNhanVienController.cs
+++ Controllers/ThongTinNhanVienController.cs
@@ -7,17 +7,21 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using BTL_Nhom12.Data;
 using BTL_Nhom12.Models;
+using BTL_Nhom12.Models.Process;
 
 namespace BTL_Nhom12.Controllers
 {
     public class ThongTinNhanVienController : Controller
     {
+        // khaibaps DbContext để làm việc với database
         private readonly ApplicationDbContext _context;
-
+        StringProcess strPro = new StringProcess();
+        //private ExcelProcess strPro = new ExcelProcess();
         public ThongTinNhanVienController(ApplicationDbContext context)
         {
             _context = context;
         }
+        //Trả về view index danh sach thongtinnhanvien
 
         // GET: ThongTinNhanVien
         public async Task<IActionResult> Index()
@@ -48,6 +52,18 @@ namespace BTL_Nhom12.Controllers
         // GET: ThongTinNhanVien/Create
         public IActionResult Create()
         {
+            var newID = "";
+            if (_context.ThongTinNhanVien.Count() == 0)
+            {
+                //khoi tao 1 ma moi
+                newID = "WORKER001";
+            }
+            else
+            {
+                var id = _context.ThongTinNhanVien.OrderByDescending(m => m.MaNhanVien).First().MaNhanVien;
+                newID = strPro.AutoGenerateKey(id);
+            }
+            ViewBag.MaNhanVien = newID;
             return View();
         }
 
@@ -154,10 +170,61 @@ namespace BTL_Nhom12.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+        //Kiểm tra xem id có tồn tại không
 
         private bool ThongTinNhanVienExists(string id)
         {
           return (_context.ThongTinNhanVien?.Any(e => e.MaNhanVien == id)).GetValueOrDefault();
         }
+        
+        //upload
+
+        //Tạo action Upload file excel lên server
+        private ExcelProcess _excelProcess = new ExcelProcess();
+        public Task<IActionResult> Upload()
+        {
+            return Task.FromResult<IActionResult>(View());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file!=null)
+        {
+            string fileExtension = Path.GetExtension(file.FileName);
+            if (fileExtension!=".xls"&& fileExtension !=".xlsx")
+            {
+                ModelState.AddModelError("","Please choose excel file to upload!");
+            }
+            else
+            {
+                //rename file when upload to server
+                var fileName = DateTime.Now.ToShortTimeString() + fileExtension;
+                var filePath = Path.Combine(Directory.GetCurrentDirectory() + "/Uploads/Excels",fileName);
+                var FileLocation = new FileInfo(filePath).ToString();
+                using (var stream = new FileStream(filePath,FileMode.Create))
+                {
+                    //save file to server
+                    await file.CopyToAsync(stream);
+                    var dt = _excelProcess.ExcelToDataTable(FileLocation);
+                        for (int i = 0; i < dt.Rows.Count; i++)
+                        {
+                            var std = new ThongTinNhanVien();
+
+                            std.MaNhanVien = dt.Rows[i][0].ToString();
+                            std.TenNhanVien= dt.Rows[i][1].ToString();
+                            std.GioiTinhNhanVien = dt.Rows[i][2].ToString();
+                            std.DiaChi = dt.Rows[i][3].ToString();
+                            std.Sdt = dt.Rows[i][4].ToString();
+
+                            _context.ThongTinNhanVien.Add(std);
+                        }
+                        await _context.SaveChangesAsync();
+                        return RedirectToAction(nameof(Index));
+                }
+            }
+        }
+        return View();
+        } 
     }
 }
